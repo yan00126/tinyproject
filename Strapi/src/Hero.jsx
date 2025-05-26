c234d8e98e145e8ee05a7e750b480fb49385fe20
yan fei <yan00126@algonquinlive.com>
2025-05-25 22:50:21 -0400

    hero component and css

--- Strapi/src/Hero.jsx
+++ Strapi/src/Hero.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>
+          Manage Any Content <br /> Anywhere
+        </h1>
+        <p>
+          Strapi is the leading open-source headless CMS. It's 100% JavaScript
+          and fully customizable. And this is just to mock Strapi
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
