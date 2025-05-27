97a60279f895d45dec90af23c1470dd8c0ca5678
yan fei <yan00126@algonquinlive.com>
2025-05-26 23:29:25 -0400

    completed version, submenu, sidebar, navbar impletemented

--- Strapi/src/Navbar.jsx
+++ Strapi/src/Navbar.jsx
@@ -2,9 +2,15 @@ import { FaBars } from "react-icons/fa";
 import { useGlobalContext } from "./context";
 import NavLinks from "./Navlinks";
 const Navbar = () => {
-  const { openSidebar } = useGlobalContext();
+  const { openSidebar, setPageId } = useGlobalContext();
+  const handleSubmenu = (e) => {
+    // console.log(e.target);
+    if (!e.target.classList.contains("nav-link")) {
+      setPageId(null);
+    }
+  };
   return (
-    <nav>
+    <nav onMouseOver={handleSubmenu}>
       <div className="nav-center">
         <h3 className="logo">strapi</h3>
         <button className="toggle-btn" onClick={openSidebar}>
--- Strapi/src/Submenu.jsx
+++ Strapi/src/Submenu.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import sublinks from "./data";
 import { useGlobalContext } from "./context";
+import { useRef } from "react";
 
 const Submenu = () => {
-  const { pageId } = useGlobalContext();
+  const { pageId, setPageId } = useGlobalContext();
   const currentPage = sublinks.find((item) => item.pageId === pageId);
+
+  const submenuContainer = useRef(null);
+
+  const handleMouseLeave = (event) => {
+    const submenu = submenuContainer.current;
+    const { left, right, bottom, top } = submenu.getBoundingClientRect();
+    const { clientX, clientY } = event;
+
+    if (clientX < left - 1 || clientX > right - 1 || clientY > bottom - 1) {
+      setPageId(null);
+    }
+    // setPageId(null);
+  };
   return (
-    <div className={currentPage ? "submenu show-submenu" : "submenu"}>
+    <div
+      className={currentPage ? "submenu show-submenu" : "submenu"}
+      onMouseLeave={handleMouseLeave}
+      ref={submenuContainer}
+    >
       <h5>{currentPage?.page}</h5>
       <div
         className="submenu-links"
