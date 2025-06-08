e8fa556fb5b3da2126f291038945ffa3ba2b3467
yan fei <yan00126@algonquinlive.com>
2025-06-08 15:43:49 -0400

    dad joke from api

--- axios/src/App.js
+++ axios/src/App.js
@@ -1,8 +1,10 @@
-import Title from './components/Title';
+import Title from "./components/Title";
+import Setup from "./examples/2-headers";
 function App() {
   return (
     <main>
       <Title />
+      <Setup />
     </main>
   );
 }
