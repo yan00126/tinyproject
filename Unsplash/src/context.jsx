4a9337e6e3291312002316926a3b5c90ab447c79
yan fei <yan00126@algonquinlive.com>
2025-06-10 17:33:33 -0400

    add toggle and darktheme in css to toggle with

--- Unsplash/src/context.jsx
+++ Unsplash/src/context.jsx
@@ -8,6 +8,9 @@ export const AppProvider = ({ children }) => {
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
+    const body = document.querySelector("body");
+    body.classList.toggle("dark-theme", newDarkTheme);
+    console.log(body);
   };
   return (
     <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
