23f1adefd2e374fa11ab72463daa11b392ed56ad
yan fei <yan00126@algonquinlive.com>
2025-06-11 18:01:27 -0400

    set up search form and handlesubmit function

--- Unsplash/src/SearchForm.jsx
+++ Unsplash/src/SearchForm.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    console.log(searchValue);
+    if (!searchValue) return;
+  };
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="dog"
+        />
+        <button className="btn" type="submit">
+          search
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
