842b1ac2b34e016bc16474f479ca8b2c4e119b72
yan fei <yan00126@algonquinlive.com>
2025-06-11 23:11:10 -0400

    unsplash api

--- Unsplash/src/Gallery.jsx
+++ Unsplash/src/Gallery.jsx
@@ -1,7 +1,55 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import axios from "axios";
+
+const url =
+  "https://api.unsplash.com/search/photos?client_id=TIJE-tt-Xabr6ZElL8oLNxUtgYxjZHvR7_BcV7MmAZk&query=british short hair";
 
 const Gallery = () => {
-  return <h2>Gallery</h2>;
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+  if (response.isError) {
+    return (
+      <section className="image-container">
+        <h4>There was an error..</h4>
+      </section>
+    );
+  }
+  const results = response.data.results;
+  if (results.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>No result found...</h4>
+      </section>
+    );
+  }
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular;
+        return (
+          <img
+            src={url}
+            alt={item.alt_description}
+            key={item.id}
+            className="img"
+          />
+        );
+      })}
+    </section>
+  );
 };
 
 export default Gallery;
