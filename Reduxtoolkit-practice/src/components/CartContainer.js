59cbd209383d5d66f1fb7728d5ad920bca2f6ce0
yan fei <yan00126@algonquinlive.com>
2025-07-27 22:22:28 -0400

    add calculateTotals to app.js to display total amount and money

--- Reduxtoolkit-practice/src/components/CartContainer.js
+++ Reduxtoolkit-practice/src/components/CartContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import CartItem from "./CartItem";
 import { useDispatch, useSelector } from "react-redux";
-import { clearCart } from "../features/cart/cartSlice";
+import { clearCart, calculateTotals } from "../features/cart/cartSlice";
 
 const CartContainer = () => {
   const dispatch = useDispatch();
@@ -32,7 +32,7 @@ const CartContainer = () => {
         <hr />
         <div className="cart-total">
           <h4>
-            total <span>${total}</span>
+            total <span>${total.toFixed(2)}</span>
           </h4>
         </div>
         <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
