ef1232a9b24e10d39e1930bd5b18ab5c92389b17
yan fei <yan00126@algonquinlive.com>
2025-07-27 22:55:16 -0400

    add Modal to confirm clear cart and add logic function

--- Reduxtoolkit-practice/src/App.js
+++ Reduxtoolkit-practice/src/App.js
@@ -1,12 +1,16 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+
 import { useDispatch, useSelector } from "react-redux";
 import { calculateTotals } from "./features/cart/cartSlice";
 import { store } from "./store";
 import { useEffect } from "react";
+import Modal from "./components/Modal";
 
 function App() {
   const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -14,6 +18,8 @@ function App() {
   }, [cartItems]);
   return (
     <main>
+      {isOpen && <Modal />}
+
       <Navbar />
       <CartContainer />
     </main>
--- Reduxtoolkit-practice/src/components/CartContainer.js
+++ Reduxtoolkit-practice/src/components/CartContainer.js
@@ -2,7 +2,8 @@ import React from "react";
 
 import CartItem from "./CartItem";
 import { useDispatch, useSelector } from "react-redux";
-import { clearCart, calculateTotals } from "../features/cart/cartSlice";
+import { clearCart } from "../features/cart/cartSlice";
+import { openModal } from "../features/modal/modalSlice";
 
 const CartContainer = () => {
   const dispatch = useDispatch();
@@ -35,7 +36,7 @@ const CartContainer = () => {
             total <span>${total.toFixed(2)}</span>
           </h4>
         </div>
-        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+        <button className="btn clear-btn" onClick={() => dispatch(openModal())}>
           clear cart
         </button>
       </footer>
