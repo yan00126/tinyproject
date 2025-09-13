341a078aeb11b6ff4f537f6a189d8d534e832650
yan fei <yan00126@algonquinlive.com>
2025-09-13 13:47:12 -0400

    UseMutation Helper, add task, with helper

--- React-query/starter/src/Form.jsx
+++ React-query/starter/src/Form.jsx
@@ -1,11 +1,21 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import customFetch from "./utils";
+import { toast } from "react-toastify";
 
 const Form = () => {
   const [newItemName, setNewItemName] = useState("");
+  const queryClient = useQueryClient();
   const { mutate: createTask, isPending } = useMutation({
     mutationFn: (taskTitle) => customFetch.post("/", { title: taskTitle }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      toast.success("task added");
+      setNewItemName("");
+    },
+    onError: (error) => {
+      toast.error(error.response.data.msg);
+    },
   });
 
   const handleSubmit = (e) => {
